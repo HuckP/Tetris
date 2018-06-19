19f96d9981ae84ef3c9f72e77f6da56a19f14a5c
2018os <tmdduq4536@naver.com>
2018-06-20 08:37:58 +0900

    dfff

--- server/app.js
+++ server/app.js
@@ -7,8 +7,6 @@ var logger = require("morgan");
 var app = express();
 // awejfiwef
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
